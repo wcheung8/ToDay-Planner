2bf7914edd2423b399f3f0762e34e018d8c435ed
Wes <we_cheung@yahoo.com>
2019-04-08 13:24:28 -0400

    initial push

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {}
