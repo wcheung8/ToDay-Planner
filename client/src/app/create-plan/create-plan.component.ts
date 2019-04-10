3f448621f0af6544c02f0a513b3829e5a1e46e18
Wes <we_cheung@yahoo.com>
2019-04-10 12:12:15 -0400

    request from server

--- client/src/app/create-plan/create-plan.component.ts
+++ client/src/app/create-plan/create-plan.component.ts
@@ -1,4 +1,14 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {HttpHeaders} from '@angular/common/http';
+import {throwError} from "rxjs/internal/observable/throwError";
+import {catchError, retry} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Component({
   selector: 'app-create-plan',
@@ -14,22 +24,49 @@ export class CreatePlanComponent implements OnInit {
   options = {
     types: ['establishment']
   };
+  log = "";
 
-  constructor() {
+  postUrl = "/api/login"
+
+  constructor(private http: HttpClient) {
 
   }
 
   addStop() {
-    this.stops.push({type: "Any", location: "", name:""});
+    this.stops.push({type: "Any", location: "", name: ""});
   }
 
   submit() {
+    this.getPlan()
+      .subscribe(plan => this.stops = plan);
+  }
 
+  getPlan() {
+    return this.http.post<any[]>(this.postUrl, this.stops, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   reset() {
-    this.stops= [];
+    this.stops = [];
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
   ngOnInit() {
   }
